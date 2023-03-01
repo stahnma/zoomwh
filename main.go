6e5805f4a9f1f2c31d80f97a1e4b139d92683e6f
Michael Stahnke <stahnma@devops.af>
2023-03-01 04:26:18 +0000

    Working MVP

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
-// TODO add basic auth since it's provided in the header
 import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
+	"log"
 	"net/http"
+	"net/url"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -48,26 +50,24 @@ type ChallengeResponse struct {
 	EncryptedToken string `json:"encryptedToken"`
 }
 
-func dostuff(c *gin.Context) {
+func processWebHook(c *gin.Context) {
 
-	/*fmt.Println("Logging to stdout I think")
-	fmt.Println(c.Params)
-	fmt.Println(c.Request) */
-
-	var jresp ZoomWebhook
-
-	if err := c.BindJSON(&jresp); err != nil {
-		// DO SOMETHING WITH THE ERROR
-		fmt.Println("There is an err", err)
+	if gin.IsDebugging() {
+		// log incoming request if in DEBUG mode
 	}
 
-	// Trying to do the CRC
+	var jresp ZoomWebhook
+	if err := c.BindJSON(&jresp); err != nil {
+		// DO SOMETHING WITH THE ERROR
+		fmt.Println("Error processing JSON.", err)
+	}
 
+	// Handle Zoom Webhook CRC validation
 	if jresp.Payload.PlainToken != "" {
 		var crc ChallengeResponse
 		crc.PlainToken = jresp.Payload.PlainToken
 		//todo load this from env var
-		zoom_secret := "020c3V6JTQKxQDucQnkXeg"
+		zoom_secret := os.Getenv("ZOOM_SECRET")
 		data := jresp.Payload.PlainToken
 		// Create a new HMAC by defining the hash type and the key (as byte array)
 		h := hmac.New(sha256.New, []byte(zoom_secret))
@@ -77,17 +77,47 @@ func dostuff(c *gin.Context) {
 		fmt.Println(crc)
 		c.JSON(http.StatusOK, crc)
 		return
-
 	}
-	fmt.Println(jresp.Event) // meeting.participant_left
-	fmt.Println(jresp.Payload.Object.Participant.UserName)
-	fmt.Println(jresp.EventTs)
 
+	var msg string
+	switch jresp.Event {
+	case "meeting.participant_left":
+		msg = jresp.Payload.Object.Participant.UserName + " has left the zoom meeting."
+	case "meeting.participant_joined":
+		msg = jresp.Payload.Object.Participant.UserName + " has joined the zoom meeting."
+	default:
+		return
+	}
+	postToSlackWebHook(msg)
+}
+
+func postToSlackWebHook(msg string) {
+
+	slack_uri := os.Getenv("ZOOM_WH_SLACK_WH_URI")
+	data := url.Values{
+		"payload": {"{\"text\": \"" + msg + "\"}"},
+	}
+	resp, err := http.PostForm(slack_uri, data)
+	fmt.Println(resp.Status)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func validateEnvVars(key string) {
+	_, ok := os.LookupEnv(key)
+	if !ok {
+		log.Fatal("You must set " + key + " environment variable.")
+	}
 }
 
 func main() {
 
+	validateEnvVars("ZOOM_SECRET")
+	validateEnvVars("ZOOM_WH_SLACK_WH_URI")
+
 	router := gin.Default()
-	router.POST("/zoom", dostuff)
-	router.Run("localhost:9999")
+	//TODO make this a configuration mount point
+	router.POST("/", processWebHook)
+	router.Run("localhost:8888")
 }
