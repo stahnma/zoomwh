c2568790cecfe2473ced43d889de99c9f3062678
Michael Stahnke <stahnke@floxdev.com>
2023-07-25 22:38:03 -0500

    Add simple IRC message sender in contrib folder
    This commit adds the simple_irc file in the contrib folder. This can be used
    for debugging connections to an IRC server and verifying you can send a
    message, have proper permissions, authenticaiton, etc. It uses the same ENV VAR
    setup as the main program, but removes the requirement of having an inbound
    webhook trigger it.

---
+++ contrib/simple_irc.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"crypto/tls"
+	"github.com/thoj/go-ircevent"
+	"log"
+	"os"
+	"time"
+)
+
+// Replace these values with your own IRC server details
+func validateEnvVars(key string) {
+	_, ok := os.LookupEnv(key)
+	if !ok {
+		log.Fatal("You must set " + key + " environment variable.")
+	}
+}
+
+func main() {
+
+	ircEnable, _ := os.LookupEnv("ZOOM_WH_IRC_ENABLE")
+	var (
+		ircServer   string
+		ircChannel  string
+		ircNick     string
+		ircPassword string
+	)
+	if ircEnable == "true" {
+		// validate all IRC variables
+		validateEnvVars("ZOOM_WH_IRC_SERVER")
+		validateEnvVars("ZOOM_WH_IRC_CHANNEL")
+		validateEnvVars("ZOOM_WH_IRC_NICK")
+		validateEnvVars("ZOOM_WH_IRC_PASS")
+
+		ircServer, _ = os.LookupEnv("ZOOM_WH_IRC_SERVER")
+		ircChannel, _ = os.LookupEnv("ZOOM_WH_IRC_CHANNEL")
+		ircNick, _ = os.LookupEnv("ZOOM_WH_IRC_NICK")
+		ircPassword, _ = os.LookupEnv("ZOOM_WH_IRC_PASS")
+	}
+
+	irccon := irc.IRC(ircNick, ircNick)
+	irccon.UseTLS = true
+	irccon.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+	irccon.Password = ircPassword
+	irccon.AddCallback("001", func(e *irc.Event) {
+		irccon.Join(ircChannel)
+	})
+
+	err := irccon.Connect(ircServer)
+	if err != nil {
+		log.Fatal("Error connecting to IRC server:", err)
+	}
+	defer irccon.Quit()
+
+	message := "Hello, IRC world! (this is an automated message)"
+	sendMessageToIRC(irccon, ircChannel, message)
+
+	time.AfterFunc(1*time.Second, func() {
+		irccon.Quit()
+	})
+
+	irccon.Loop()
+}
+
+func sendMessageToIRC(irccon *irc.Connection, channel, message string) {
+	irccon.Privmsg(channel, message)
+}
