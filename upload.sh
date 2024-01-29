891d933ee611b63b8965e81dddcf15507a3c6203
Michael Stahnke <stahnma@devops.af>
2024-01-28 21:41:33 -0600

    Randomize the image for testing so it's more fun.

--- upload.sh
+++ upload.sh
@@ -1,7 +1,10 @@
 #curl -X POST -F "image=@/path/to/your/image.jpg" -F "caption=OptionalCaption" http://localhost:8080/upload
 
+file=`ls ~/Desktop/*.png ~/Desktop/*.jpeg |sort -R |tail -1`
+echo $file
+
 curl -X POST \
-  -F "image=@./image.png" \
+  -F "image=@$file" \
   -F "caption=OptionalCaption"  \
   -H "X-API-KEY: abc123" \
 http://localhost:7171/upload
