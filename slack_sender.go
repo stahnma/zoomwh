2ae3c2d90351e79dbf4449d900333f6320e321dd
Michael Stahnke <mastahnke@gmail.com>
2024-02-02 23:15:07 -0600

    chore: flip all logging to logrus for slack_sender

--- slack_sender.go
+++ slack_sender.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
@@ -22,16 +20,16 @@ var (
 )
 
 func watchDirectory(directoryPath string, done chan struct{}) {
-	log.Debugln("Inside watchDirectory", directoryPath)
+	log.Debugln("(watchDirectory)", directoryPath)
 	if err := watcher.Add(directoryPath); err != nil {
-		fmt.Println("Error watching directory:", err)
+		log.Errorln("Error watching directory:", err)
 		return
 	}
 	for {
 		select {
 		case event, ok := <-watcher.Events:
 			if !ok {
-				fmt.Println("watcher.Events channel closed. Exiting watchDirectory.")
+				log.Debugln("(watchingDirectory) watcher.Events channel closed. Exiting watchDirectory.")
 				return
 			}
 
@@ -41,13 +39,13 @@ func watchDirectory(directoryPath string, done chan struct{}) {
 
 		case err, ok := <-watcher.Errors:
 			if !ok {
-				fmt.Println("watcher.Errors channel closed. Exiting watchDirectory.")
+				log.Debugln("(watchDirectory) watcher.Errors channel closed. Exiting watchDirectory.")
 				return
 			}
-			fmt.Println("Error watching directory:", err)
+			log.Errorln("Error watching directory:", err)
 
 		case <-done:
-			fmt.Println("Received signal to exit. Exiting watchDirectory.")
+			log.Debugln("(watchDirectory) Received signal to exit. Exiting watchDirectory.")
 			return
 		}
 
@@ -57,7 +55,7 @@ func watchDirectory(directoryPath string, done chan struct{}) {
 func handleNewFile(filePath string) {
 	// if it's not an image, use the json file to see comment
 
-	log.Debugln("Inside handleNewFile", filePath)
+	log.Debugln("(handleNewFile) filePath:", filePath)
 	mu.Lock()
 	defer mu.Unlock()
 
@@ -66,18 +64,18 @@ func handleNewFile(filePath string) {
 	}
 
 	if isJson(filePath) {
-		log.Debugln("Found a json file", filePath)
+		log.Debugln("(handleNewfile) Found a json file", filePath)
 		var j ImageInfo
 		var err error
-		content, err := ioutil.ReadFile(filePath)
+		content, err := os.ReadFile(filePath)
 		if err != nil {
-			log.Debugln("I can't read the file", filePath)
+			log.Debugln("(handleNewFile) Can't read the file", filePath)
 		}
 		err = json.Unmarshal(content, &j)
 		if err != nil {
 			// move the file to the invalid folder
 			log.Warnln("Unable to process " + filePath + " moving to invalid folder")
-			log.Debugln("Json unmarhsalling error is ", err)
+			log.Debugln("(handleNewFile) Json unmarhsalling error is ", err)
 			return
 		}
 		handleImageFile(j)
@@ -93,7 +91,7 @@ func handleImageFile(j ImageInfo) {
 		// Upload the new image to Slack
 		err := uploadImageToSlack(api, j, viper.GetString("slack_channel"))
 		if err != nil {
-			fmt.Printf("File %s not uploaded. Error: %v\n", filepath.Base(filePath), err)
+			log.Errorln("File %s not uploaded. Error: %v\n", filepath.Base(filePath), err)
 			return
 		}
 		moveToDir(filePath, viper.GetString("processed_dir"))
@@ -161,32 +159,32 @@ func hasJsonExtension(fileName string) bool {
 
 // FIXME - handle sceanrio where a spare image is just in the upload dir
 func handleSpareImage(filePath string) {
-	log.Debugln("Inside handleSpareImage", filePath)
+	log.Debugln("(handleSpareImage) filePath: ", filePath)
 	mu.Lock()
 	defer mu.Unlock()
 	moveToDir(filePath, viper.GetString("discard_dir"))
 }
 
 func isJson(filename string) bool {
-	log.Debugln("Inside isJson", filename)
+	log.Debugln("(isJson) filename:", filename)
 	if hasJsonExtension(filename) {
 		log.Debugln(filename + " Has Json extension")
-		content, err := ioutil.ReadFile(filename)
+		content, err := os.ReadFile(filename)
 		if err != nil {
-			log.Debugln("Unable to read file", filename)
+			log.Debugln("(isJson) Unable to read file", filename)
 			return false
 		}
 		// See if it's valid JSON
 		var jsonData interface{}
 		err = json.Unmarshal(content, &jsonData)
 		if err != nil {
-			log.Debugln("Unable to parse json. Error is", err)
+			log.Debugln("(isJson) Unable to parse json. Error is", err)
 			moveToDir(filename, viper.GetString("discard_dir"))
 			log.Infoln("Moved " + filename + " to discard directory. Invalid JSON file or schema.")
 			return false
 		}
 		return true
 	}
-	log.Debugln(filename + "Not a json file")
+	log.Debugln("(isJson) " + filename + "Not a json file")
 	return false
 }
