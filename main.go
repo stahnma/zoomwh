7d996e38d476c46e249651f88d1ad5ee3df56148
Michael Stahnke <stahnma@devops.af>
2024-02-03 22:13:43 -0600

    feat: Switch to viper for configuration parsing.
    Previously the parsing of configuration was done rather manually through hefty
    usage of os.GetEnv and the like. This switches to Viper which is easier to work
    with and allows for future expansion into a configuration file or other
    options. It's also pretty standard.
    
    At the same time the logger was flipped to logrus.

--- main.go
+++ main.go
@@ -5,13 +5,14 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 )
 
 type ZoomWebhook struct {
@@ -50,6 +51,53 @@ type ChallengeResponse struct {
 	EncryptedToken string `json:"encryptedToken"`
 }
 
+func zoomCrcValidation(jresp ZoomWebhook) (bool, ChallengeResponse) {
+	log.Debugln("(zoomCrcValidation)")
+	zoom_secret := viper.GetString("zoom_secret")
+	var crc ChallengeResponse
+	if jresp.Payload.PlainToken != "" {
+		crc.PlainToken = jresp.Payload.PlainToken
+		data := jresp.Payload.PlainToken
+		// Create a new HMAC by defining the hash type and the key (as byte array)
+		h := hmac.New(sha256.New, []byte(zoom_secret))
+		h.Write([]byte(data))
+		// Get result and encode as hexadecimal string
+		crc.EncryptedToken = hex.EncodeToString(h.Sum(nil))
+		log.Infoln("CRC Validation: ", crc)
+
+		return true, crc
+	}
+	return false, crc
+
+}
+
+func applyMeetingFilters(jresp ZoomWebhook) bool {
+	// If the meeting is outside the topic scope, just ignore.
+	name := viper.GetString("meeting_name")
+	fmt.Println("Topic " + jresp.Payload.Object.Topic)
+	if name != jresp.Payload.Object.Topic && name != "" {
+		log.Infoln("Received hook but dropping due to topic being filtered.")
+		log.Debugln("(applyMeetingFilter) Hook had topic '" + jresp.Payload.Object.Topic + "'")
+		log.Debugln("(applyMeetingFtiler)Filter only allows for " + name)
+		return true
+	}
+	return false
+}
+
+func setMessageSuffix(jresp ZoomWebhook) string {
+	msg_suffix := viper.GetString("msg_suffix")
+	msg := ""
+	switch jresp.Event {
+	case "meeting.participant_left":
+		msg = jresp.Payload.Object.Participant.UserName + " has left " + msg_suffix
+	case "meeting.participant_joined":
+		msg = jresp.Payload.Object.Participant.UserName + " has joined " + msg_suffix
+	default:
+		return msg
+	}
+	return msg
+}
+
 func processWebHook(c *gin.Context) {
 
 	if gin.IsDebugging() {
@@ -57,68 +105,44 @@ func processWebHook(c *gin.Context) {
 	}
 	var jresp ZoomWebhook
 	if err := c.BindJSON(&jresp); err != nil {
-		// DO SOMETHING WITH THE ERROR
-		fmt.Println("Error processing JSON.", err)
+		log.Errorln("Error processing incoming webhook JSON", err)
 	}
 
 	// Handle Zoom Webhook CRC validation
 	if jresp.Payload.PlainToken != "" {
-		var crc ChallengeResponse
-		crc.PlainToken = jresp.Payload.PlainToken
-		//todo load this from env var
-		zoom_secret := os.Getenv("ZOOM_SECRET")
-		data := jresp.Payload.PlainToken
-		// Create a new HMAC by defining the hash type and the key (as byte array)
-		h := hmac.New(sha256.New, []byte(zoom_secret))
-		h.Write([]byte(data))
-		// Get result and encode as hexadecimal string
-		crc.EncryptedToken = hex.EncodeToString(h.Sum(nil))
-		fmt.Println(crc)
-		c.JSON(http.StatusOK, crc)
+		crcvalid, crc := zoomCrcValidation(jresp)
+		if crcvalid {
+			log.Debugln("(processWebHook) CRC validation successful. Returning CRC response.")
+			c.JSON(http.StatusOK, crc)
+			return
+		} else {
+			log.Errorln("(processWebHook) CRC validation failed. Returning 400.")
+			c.JSON(http.StatusBadRequest, gin.H{"error": "CRC validation failed"})
+			return
+		}
+	}
+	if applyMeetingFilters(jresp) {
 		return
 	}
 
-	// If the meeting is outside the topic scope, just ignore.
-	name, _ := os.LookupEnv("ZOOMWH_MEETING_NAME")
-	fmt.Println("Topic " + jresp.Payload.Object.Topic)
-	if name != jresp.Payload.Object.Topic && name != "" {
-		fmt.Println("Received hook but dropping due to topic.")
-		fmt.Println("Hook had topic '" + jresp.Payload.Object.Topic + "'")
-		fmt.Println("Filter only allows for " + name)
-		return
-	}
+	msg := setMessageSuffix(jresp)
+	dispatchMessage(msg)
+}
 
-	msg_suffix := os.Getenv("ZOOMWH_MSG_SUFFIX")
-	if msg_suffix == "" {
-		msg_suffix = "the zoom meeting."
-	}
+func dispatchMessage(msg string) {
 
-	var msg string
-	switch jresp.Event {
-	case "meeting.participant_left":
-		msg = jresp.Payload.Object.Participant.UserName + " has left " + msg_suffix
-	case "meeting.participant_joined":
-		msg = jresp.Payload.Object.Participant.UserName + " has joined " + msg_suffix
-	default:
-		return
-	}
-
-	slack_enable := os.Getenv("ZOOMWH_SLACK_ENABLE")
-	irc_enable := os.Getenv("ZOOMWH_IRC_ENABLE")
+	slack_enable := viper.GetString("slack_enable")
+	irc_enable := viper.GetString("irc_enable")
 	sent := 0
 
-	if strings.ToLower(irc_enable) == "true" {
-
-	}
-
 	if strings.ToLower(slack_enable) == "true" {
-		log.Println("Sending a slack message")
+		log.Debugln("(dispatchMessage) Sending a slack message")
 		postToSlackWebHook(msg)
 		sent = 1
 
 	}
 	if strings.ToLower(irc_enable) == "true" {
-		log.Println("Sending an IRC message")
+		log.Debugln("(dispatchMessage) Sending an IRC message")
 		sendIRC(msg)
 		sent = 1
 	}
@@ -127,28 +151,85 @@ func processWebHook(c *gin.Context) {
 	}
 }
 
-func validateEnvVars(key string) {
-	_, ok := os.LookupEnv(key)
-	if !ok {
-		log.Fatal("You must set " + key + " environment variable.")
+func init() {
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.DebugLevel)
+
+	viper.SetDefault("port", "2003")
+	viper.SetDefault("slack_enable", "true")
+	viper.SetDefault("irc_enable", "false")
+	viper.SetDefault("msg_suffix", "the zoom meeting.")
+
+	viper.BindEnv("port", "ZOOMWH_PORT")
+
+	bugout := false
+	if value := os.Getenv("ZOOM_SECRET"); value == "" {
+		bugout = true
+		log.Errorln("You must set ZOOM_SECRET environment variable.")
+	} else {
+		viper.BindEnv("zoom_secret", "ZOOM_SECRET")
+	}
+
+	// Slack Specifics
+	if value := os.Getenv("ZOOMWH_SLACK_ENABLE"); value == "false" {
+		log.Infoln("Slack is notification disabled.")
+	} else {
+		viper.BindEnv("slack_webhook_uri", "ZOOMWH_SLACK_WH_URI")
+	}
+
+	if value := os.Getenv("ZOOMWH_MEETING_NAME"); value == "" {
+		viper.BindEnv("meeting_filter", "ZOOMWH_MEETING_NAME")
+	}
+
+	// IRC Specifics
+	value, ok := os.LookupEnv("ZOOMWH_IRC_ENABLE")
+	if value == "false" || !ok {
+		log.Infoln("IRC notifications are disabled.")
+		viper.Set("irc_enabled", "false")
+	} else {
+		// Four IRC variables are required if IRC is enabled
+		if value := os.Getenv("ZOOMWH_IRC_SERVER"); value == "" {
+			log.Errorln("You must set ZOOMWH_IRC_SERVER environment variable if ZOOMWH_IRC_ENABLE is true.")
+			bugout = true
+		} else {
+			viper.MustBindEnv("irc_server", "ZOOMWH_IRC_SERVER")
+		}
+		if value := os.Getenv("ZOOMWH_IRC_CHANNEL"); value == "" {
+			log.Errorln("You must set ZOOMWH_IRC_CHANNEL environment variable if ZOOMWH_IRC_ENABLE is true.")
+			bugout = true
+		} else {
+			viper.MustBindEnv("irc_channel", "ZOOMWH_IRC_CHANNEL")
+		}
+		if value := os.Getenv("ZOOMWH_IRC_NICK"); value == "" {
+			log.Errorln("You must set ZOOMWH_IRC_NICK environment variable if ZOOMWH_IRC_ENABLE is true.")
+			bugout = true
+		} else {
+			viper.MustBindEnv("irc_nick", "ZOOMWH_IRC_NICK")
+		}
+		if value := os.Getenv("ZOOMWH_IRC_PASS"); value == "" {
+			log.Errorln("You must set ZOOMWH_IRC_PASS environment variable if ZOOMWH_IRC_ENABLE is true.")
+			bugout = true
+		} else {
+			viper.MustBindEnv("irc_pass", "ZOOMWH_IRC_PASS")
+		}
+	}
+
+	viper.MustBindEnv("zoom_secret", "ZOOM_SECRET")
+	if os.Getenv("ZOOMWH_MSG_SUFFIX") != "" {
+		viper.BindEnv("msg_suffix", "ZOOMWH_MSG_SUFFIX")
+	}
+
+	if bugout == true {
+		os.Exit(1)
 	}
 }
 
 func main() {
 
-	validateEnvVars("ZOOM_SECRET")
-
 	router := gin.Default()
-	//TODO make this a configuration mount point
 	router.POST("/", processWebHook)
-	port, set := os.LookupEnv("ZOOMWH_PORT")
-	if set {
-		port = os.Getenv("ZOOMWH_PORT")
-	} else {
-		port = "8888"
-	}
-
+	port := viper.GetString("port")
 	serverstring := "localhost:" + port
-	fmt.Println("Listening on " + serverstring)
+	log.Infoln("Listening on " + serverstring)
 	router.Run(serverstring)
 }
