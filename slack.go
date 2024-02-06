2ae44212504ff4647ff256ad398aa92e53a03de6
Michael Stahnke <stahnma@devops.af>
2024-02-05 23:33:03 -0600

    Feat: Multiplex slack notifications
    This commit allows for a comma separated list of slack webhooks to be used as
    notification endpoints for what's happening in zoom. This is helpful if you
    have one zoom where you want to notify multiple slack teams or channels. Just
    create the webhook and put the URI in the `ZOOMWH_SLACK_WEBHOOK_URI`
    environment variable.
    
    Note: You still have exaclty 1 IRC endpoint to send and that probably won't be
    changed any time in the near future.

--- slack.go
+++ slack.go
@@ -3,20 +3,33 @@ package main
 import (
 	"net/http"
 	"net/url"
+	"strings"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
-func postToSlackWebHook(msg string) {
-	log.Debugln("(postToSlackWebHook) Sending slack message: " + "'" + msg + "'")
+func parseAndSplitSlackHooks(msg string) {
+	log.Debugln("(parseAndSplitSlackHooks) Parsing and splitting slack hooks.")
+	slackHooks := viper.GetString("slack_webhook_uri")
 
-	slack_webhook_uri := viper.GetString("slack_webhook_uri")
-	log.Debugln("(postToSlackWebHook) slack_webhook_uri:", slack_webhook_uri)
+	splitStrings := strings.Split(slackHooks, ",")
+	for i, s := range splitStrings {
+		splitStrings[i] = strings.ReplaceAll(s, "'", "")
+	}
+	size := len(splitStrings)
+	log.Debugln("(parseAndSplitSlackHooks) Found", size, "slack hooks.")
+	for _, entry := range splitStrings {
+		postToSlack(msg, entry)
+	}
+}
+
+func postToSlack(msg string, uri string) {
+	log.Debugln("(postToSlack) Posting to each slack hook.", uri, msg)
 	data := url.Values{
 		"payload": {"{\"text\": \"" + msg + "\"}"},
 	}
-	resp, err := http.PostForm(slack_webhook_uri, data)
+	resp, err := http.PostForm(uri, data)
 	if err != nil {
 		log.Errorln("Error posting to slack:", err)
 	}
